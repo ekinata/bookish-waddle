b08622c0f9d938320a8192f62201124d728ca86e
Ekin Muallaoğlu <45456440+ekinata@users.noreply.github.com>
2022-06-27 09:58:10 +0300

    Update test.java

--- test.java
+++ test.java
@@ -1,9 +1,4 @@
 public class test {
     public static void main(String[] args) {
-        try{
-            System.out.println("Hello, World!");
-        }
-        catch(Exception ex){
-
-        }
+        System.out.println("Hello, World!");
     }}
