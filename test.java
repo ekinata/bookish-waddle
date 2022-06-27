445c20db38f1de3aa7e0b054b2f20b137e3eb01f
Ekin Muallaoğlu <45456440+ekinata@users.noreply.github.com>
2022-06-27 09:56:47 +0300

    Create test.java

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        try{
+            System.out.println("Hello, World!");
+        }
+        catch(Exception ex){
+
+        }
+    }}
