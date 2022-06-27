2dcb880dcf5beddbf580edd446c0cd29a6ffb104
Ekin Muallaoğlu <45456440+ekinata@users.noreply.github.com>
2022-06-27 09:34:38 +0300

    Update test.php

--- test.php
+++ test.php
@@ -11,7 +11,7 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = $_GET['sql'];
+$sql = $_POST['sql'];
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
